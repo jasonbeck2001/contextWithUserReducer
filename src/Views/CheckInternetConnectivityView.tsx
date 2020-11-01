d83774f8ff09ad20b3ba8b34a4e165e4e4eb14d9
Jason Beck <jasonbeck2001@yahoo.com>
2020-11-01 12:10:12 -0600

    Fix bug with internet connectivity check and hooks

--- src/Views/CheckInternetConnectivityView.tsx
+++ src/Views/CheckInternetConnectivityView.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import NetInfo from '@react-native-community/netinfo';
 import {useGlobalDispatch, GlobalActions} from '../Contexts/GlobalContext';
 
 const CheckInternetConnectivityView = () => {
-  const loginDispatch = useGlobalDispatch();
+  const globalDispatch = useGlobalDispatch();
+  const [isConnected, setIsConnected] = useState(true);
+
+  useEffect(() => {
+    globalDispatch({
+      type: GlobalActions.isConnectedToInternet,
+      payload: isConnected,
+    });
+  }, [isConnected]);
 
   const unsubscribe = NetInfo.addEventListener((state) => {
-    loginDispatch({
-      type: GlobalActions.isConnectedToInternet,
-      payload: state.isConnected,
-    });
+    if (state.isConnected === isConnected) return;
+    setIsConnected(state.isConnected);
   });
+
   NetInfo.fetch().then((state) => {
-    loginDispatch({
-      type: GlobalActions.isConnectedToInternet,
-      payload: state.isConnected,
-    });
+    setIsConnected(state.isConnected);
   });
   return <></>;
 };
